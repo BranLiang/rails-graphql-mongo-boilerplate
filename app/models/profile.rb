1f40fe4732e6f60e31d956f35a3943cd2e7e878b
Bran Liang <lby89757@hotmail.com>
2017-08-31 13:41:47 +0800

    add name validation on user

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,7 @@
 class Profile
   include Mongoid::Document
   include Mongoid::Timestamps
-
   embedded_in :user
-
   field :gender, type: String, default: 'male'
-
   validates :gender, inclusion: { in: %w(male female) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
-
   embeds_one :profile
-
   field :name, type: String
+  validates :name, length: { in: 2..8 }, format: { with: /\p{Han}/u }
 end
