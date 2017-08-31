e1a5b50f2b2841ba16ecbe74764a15f2c583cd92
Bran Liang <lby89757@hotmail.com>
2017-08-31 12:23:40 +0800

    add profile model

---
+++ app/models/profile.rb
@@ -0,0 +1,10 @@
+class Profile
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  embedded_in :user
+
+  field :gender, type: String, default: 'male'
+
+  validates :gender, inclusion: { in: %w(male female) }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  embeds_one :profile
+
   field :name, type: String
 end
